24a695be4029ce6b56d2cd76c1f6f608b7c6b130
Diwakar Kumar <diwkumar1@publicisgroupe.net>
2020-04-01 18:20:18 +0530

    Docs updated

--- myts-app/src/Assignment1.ts
+++ myts-app/src/Assignment1.ts
@@ -1,35 +1,44 @@
 
 
+// 1.Demonstrate how to use classes and interfaces
+// Have common interface called “Calculator”
+// 1.Implement  with InterestCalculator , show interest calculation
+// 2.Implemement with “ShapeCalculator” ,Show shape calculation. 
+
+// Calcultor Interface
 interface Calculator {
-    calcullate (): void;
+    calculate (): void;
 }
 
+// InterestCalculator class which implemet Calculator method and give
+// defination to calculate method .
 class InterestCalculator  implements Calculator {
-
-    
- constructor (public principle: number, public rate: number , public time: number) {
+    constructor (public principle: number, public rate: number , public time: number) {
     
 }
-    public calcullate(): void {
+    // Calculate simple Interest
+    public calculate(): void {
         const interest = (this.principle * this.rate * this.time) / 100;
         console.log(`Interest = ${interest}`);
     }
 }
 
+// ShapeCalculator class which implemet Calculator method and give
+// defination to calculate method .
 class ShapeCalculator  implements Calculator {
-
     constructor (public length: number, public breath: number) {
     
     }
-        public calcullate(): void {
-            const area = (this.length * this.breath );
-            console.log(`Shape Area = ${area}`);
-        }
+     // Calculate Area of shape
+    public calculate(): void {
+        const area = (this.length * this.breath );
+        console.log(`Shape Area = ${area}`);
+    }
 }
 
 
 const interest = new InterestCalculator(1000, 5, 2);
-interest.calcullate();
+interest.calculate();
 
 let shape = new ShapeCalculator(10, 20);
-shape.calcullate();
+shape.calculate();
